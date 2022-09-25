eb7ea512a626e896711317c8901cf4fa390f4698
3145tttt <3145tttt002@mail.ru>
2022-09-25 15:22:51 +0300

    test

--- statphys/Form1.Designer.cs
+++ statphys/Form1.Designer.cs
@@ -36,7 +36,16 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label_mean = new System.Windows.Forms.Label();
+            this.label_var = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.numericUpDownMean = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownVar = new System.Windows.Forms.NumericUpDown();
+            this.label_count = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownMean)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownVar)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
@@ -60,6 +69,7 @@
             // 
             // timer1
             // 
+            this.timer1.Interval = 50;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // label1
@@ -79,7 +89,7 @@
             this.button2.TabIndex = 3;
             this.button2.Text = "Тест";
             this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button2.Click += new System.EventHandler(this.button_numeric_methods_Click);
             // 
             // button3
             // 
@@ -99,13 +109,95 @@
             this.button4.TabIndex = 5;
             this.button4.Text = "start loop";
             this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
+            this.button4.Click += new System.EventHandler(this.button_start_timer_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(363, 380);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "reset";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button_reset_Click);
+            // 
+            // label_mean
+            // 
+            this.label_mean.AutoSize = true;
+            this.label_mean.Location = new System.Drawing.Point(37, 224);
+            this.label_mean.Name = "label_mean";
+            this.label_mean.Size = new System.Drawing.Size(57, 15);
+            this.label_mean.TabIndex = 7;
+            this.label_mean.Text = "Mean = 0";
+            // 
+            // label_var
+            // 
+            this.label_var.AutoSize = true;
+            this.label_var.Location = new System.Drawing.Point(37, 265);
+            this.label_var.Name = "label_var";
+            this.label_var.Size = new System.Drawing.Size(71, 15);
+            this.label_var.TabIndex = 8;
+            this.label_var.Text = "Variance = 0";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(363, 341);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "stoop loop";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button_stop_timer_Click);
+            // 
+            // numericUpDownMean
+            // 
+            this.numericUpDownMean.DecimalPlaces = 1;
+            this.numericUpDownMean.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.numericUpDownMean.Location = new System.Drawing.Point(37, 341);
+            this.numericUpDownMean.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDownMean.Name = "numericUpDownMean";
+            this.numericUpDownMean.Size = new System.Drawing.Size(120, 23);
+            this.numericUpDownMean.TabIndex = 10;
+            this.numericUpDownMean.ThousandsSeparator = true;
+            this.numericUpDownMean.ValueChanged += new System.EventHandler(this.numericUpDownMean_ValueChanged);
+            // 
+            // numericUpDownVar
+            // 
+            this.numericUpDownVar.Location = new System.Drawing.Point(37, 380);
+            this.numericUpDownVar.Name = "numericUpDownVar";
+            this.numericUpDownVar.Size = new System.Drawing.Size(120, 23);
+            this.numericUpDownVar.TabIndex = 11;
+            this.numericUpDownVar.ValueChanged += new System.EventHandler(this.numericUpDownVar_ValueChanged);
+            // 
+            // label_count
+            // 
+            this.label_count.AutoSize = true;
+            this.label_count.Location = new System.Drawing.Point(37, 302);
+            this.label_count.Name = "label_count";
+            this.label_count.Size = new System.Drawing.Size(60, 15);
+            this.label_count.TabIndex = 12;
+            this.label_count.Text = "Count = 0";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label_count);
+            this.Controls.Add(this.numericUpDownVar);
+            this.Controls.Add(this.numericUpDownMean);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.label_var);
+            this.Controls.Add(this.label_mean);
+            this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -115,6 +207,8 @@
             this.Name = "Form1";
             this.Text = "statphys";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownMean)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownVar)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -129,5 +223,12 @@
         private Button button2;
         private Button button3;
         private Button button4;
+        private Button button5;
+        private Label label_mean;
+        private Label label_var;
+        private Button button6;
+        private NumericUpDown numericUpDownMean;
+        private NumericUpDown numericUpDownVar;
+        private Label label_count;
     }
 }
--- statphys/Form1.cs
+++ statphys/Form1.cs
@@ -1,72 +1,190 @@
 using System.Diagnostics;
+using System.Globalization;
 using System.Numerics;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace statphys
 {
     public partial class Form1 : Form
     {
+        List<double> points_x = new List<double> { 0 };
+        List<double> delta_arr = new List<double> { };
+        Random rnd;
+        const bool DEBUG = true;
+        double mean = 0, var = 10;
+
         public Form1()
         {
             InitializeComponent();
+
+            set_NumericUpDown();
+
+            //init Random gen U(0, 1)
+            rnd = new Random(12);
+            //init debug logger
+            Trace.Listeners.Add(new TextWriterTraceListener("debugOut.log"));
+            Trace.AutoFlush = true;
+        }
+
+        // Start timer
+        private void button_start_timer_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void button_stop_timer_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            get_new_sample();
-            pictureBox1.Refresh();
+            get_new_iteration();
         }
 
-        int x = 0;
+        // Manually sampling
         private void button_update_Click(object sender, EventArgs e)
+        {
+            get_new_iteration();
+        }
+
+        private void numericUpDownMean_ValueChanged(object sender, EventArgs e)
+        {
+            reset();
+            mean = (double)numericUpDownMean.Value;
+        }
+
+        private void numericUpDownVar_ValueChanged(object sender, EventArgs e)
+        {
+            reset();
+            var = (double)numericUpDownVar.Value;
+        }
+
+        void get_new_iteration()
         {
             get_new_sample();
             pictureBox1.Refresh();
+            update_mean_var_labels();
         }
 
         void get_new_sample()
         {
-            int delta = sample_delta();
-            x += delta;
-            points_x.Add(x);
+            double delta = sample_delta();
+            double cur_x = points_x.Last() + delta;
+            points_x.Add(cur_x);
+            delta_arr.Add(delta);
+            update_label(cur_x, delta);
+
+            if (DEBUG)
+            {
+                Trace.WriteLine(delta.ToString(CultureInfo.GetCultureInfo("en-GB")));
+            }
+        }
+
+        void update_label(double x, double delta)
+        {
             label1.Text = $"x = {x} delta = {delta}";
             label1.Refresh();
         }
-        List<int> points_x = new List<int>{0};
-        int sample_delta()
+
+        void update_mean_var_labels()
         {
-            double mean = 0, stdDev = 10;
-            Random rand = new Random(); //reuse this if you are generating many
-            double u1 = 1.0 - rand.NextDouble(); //uniform(0,1] random doubles
-            double u2 = 1.0 - rand.NextDouble();
-            double randStdNormal = Math.Sqrt(-2.0 * Math.Log(u1)) *
-                         Math.Sin(2.0 * Math.PI * u2); //random normal(0,1)
-            double randNormal =
-                mean + stdDev * randStdNormal; //random normal(mean,stdDev^2)
-            Console.WriteLine(randNormal);
-            return Convert.ToInt32(Math.Round(randNormal));
+            double mean = 0, var = 0;
+            get_mean_var(ref mean, ref var, delta_arr);
+
+            label_mean.Text = $"Mean = {mean}";
+            label_var.Text = $"Variance = {var}";
+            label_count.Text = $"Count = {delta_arr.Count()}";
+
+            label_var.Refresh();
+            label_mean.Refresh();
+            label_count.Refresh();
         }
 
+        double sample_delta()
+        {
+            double val = rnd.NextDouble();
+
+            const double eps = 0.00001;
+            const int n = 4000;
+
+            // Find cdf(delta) = val
+            double sigma = Math.Sqrt(var);
+            double a = -10*sigma, b = 10*sigma;
+
+            Func<double, double> normal = x => normal_dist(x, mean, Math.Sqrt(var));
+            Func<double, double> get_cdf = x => integrate_numeric(normal, a, x, n);
+            double delta = find_root(get_cdf, val, a, b, n, eps);
+
+            // Check cdf(delta) == val
+            Debug.Assert(Math.Abs(val - integrate_numeric(normal, a, delta, n*10)) < eps);
+            return delta;
+        }
+
+        double normal_dist(double x, double m, double std)
+        {
+            double exponent = (x - m) * (x - m) / (std * std);
+            return 1.0 / (std * Math.Sqrt(2 * Math.PI)) * Math.Exp(-exponent / 2.0);
+        }
+
+        // Paint line and points
         private void pictureBox1_Paint(object sender, PaintEventArgs e)
         {
+            // Line
             int HIEGHT_2 = pictureBox1.Size.Height / 2, WIDTH_2 = pictureBox1.Size.Width / 2;
             int POINT_HIEGHT = 10, POINT_WIDTH = 10;
             e.Graphics.DrawLine(
                 new Pen(Color.Red, 2f),
                 new Point(0, HIEGHT_2),
                 new Point(pictureBox1.Size.Width, HIEGHT_2));
+
+            // Points
             SolidBrush commonBrush = new SolidBrush(Color.Orange);
             SolidBrush lastBrush = new SolidBrush(Color.Blue);
-
-            for (int i = 0; i < points_x.Count - 1; ++i)
+            for (int i = 0; i < points_x.Count; ++i)
             {
-                e.Graphics.FillEllipse(commonBrush,
-                WIDTH_2 + points_x[i], HIEGHT_2 - POINT_HIEGHT / 2, POINT_WIDTH, POINT_HIEGHT);
+                int x = Convert.ToInt32(points_x[i]);
+                SolidBrush brush = i == points_x.Count - 1 ? lastBrush : commonBrush;
+                e.Graphics.FillEllipse(brush, WIDTH_2 + x, HIEGHT_2 - POINT_HIEGHT / 2, POINT_WIDTH, POINT_HIEGHT);
             }
-            e.Graphics.FillEllipse(lastBrush,
-            WIDTH_2 + points_x.Last(), HIEGHT_2 - POINT_HIEGHT / 2, POINT_WIDTH, POINT_HIEGHT);
         }
 
-        //Численное нахождение корня и тесты
+        // Test numeric methods
+        private void button_numeric_methods_Click(object sender, EventArgs e)
+        {
+            test_integrator();
+            test_root();
+        }
+
+        // Test N sampling from distrubition;
+        // out => statphys\statphys\bin\Debug\net6.0-windows\debugOut.log
+        private void button3_Click(object sender, EventArgs e)
+        {
+            test_sampling();
+        }
+
+        // Reset programm
+        private void button_reset_Click(object sender, EventArgs e)
+        {
+            reset();
+
+        }
+
+        void reset()
+        {
+            timer1.Stop();
+            points_x = new List<double> { 0 };
+            delta_arr = new List<double> { };
+
+            update_label(0, 0);
+            pictureBox1.Refresh();
+            update_mean_var_labels();
+        }
+
+        // Numeric find root
+        // f(x) = y;
+        // f'(x) >= 0;
+        // x in [a, b];
         double find_root(Func<double, double> f, double y, double a, double b, int n, double eps)
         {
             double l = a, r = b;
@@ -95,8 +213,64 @@ namespace statphys
             return x;
         }
 
+        // Numeric intregral
+        // integral{b}{a}f(x)dx = ...
+        double integrate_numeric(Func<double, double> f, double a, double b, int n)
+        {
+            double h = (b - a) / n;
+            double S = 0;
+            for (int i = 0; i < n; ++i)
+            {
+                double x1 = a + i * h;
+                double x2 = a + (i + 1) * h;
+                // Simpson's rule https://habr.com/ru/post/479202/
+                S += h / 6.0 * (f(x1) + 4.0 * f((x1 + x2) / 2) + f(x2));
+            }
+            return S;
+        }
+
+        void get_mean_var(ref double mean, ref double var, List<double> arr)
+        {
+            if(arr.Count() == 0)
+            {
+                mean = 0;
+                var = 0;
+                return;
+            }
+            mean = arr.Average();
+            var = get_var(arr, mean);
+        }
+
+        // Calculate var
+        double get_var(List<double> arr, double mean)
+        {
+            return arr.Sum(x => (x - mean) * (x - mean)) / arr.Count();
+        }
+
+        // Set property NumercicUpDown mean var
+        void set_NumericUpDown()
+        {
+            this.numericUpDownMean.Maximum = 100;
+            this.numericUpDownMean.Minimum = -100;
+            this.numericUpDownMean.ThousandsSeparator = true;
+            this.numericUpDownMean.DecimalPlaces = 1;
+            this.numericUpDownMean.Increment = new decimal(0.1);
+
+            this.numericUpDownVar.Maximum = 100;
+            this.numericUpDownVar.Minimum = 1;
+            this.numericUpDownVar.ThousandsSeparator = true;
+            this.numericUpDownVar.DecimalPlaces = 1;
+            this.numericUpDownVar.Increment = new decimal(0.1);
+        }
+
+        // Test numeric root f(x) = y;
         void test_root()
         {
+
+            Func<double, double> g1 = x => 1 - Math.Exp(-x);
+            Func<double, double> g2 = x => -(Math.Sin(x) + Math.Cos(x));
+            Func<double, double> g3 = x => Math.Exp(x * x);
+
             double r1 = find_root(g1, 0.2, 0.1, 100, 1000, 0.000001);
             double r2 = find_root(g2, 0.2, 1, 3, 1000, 0.000001);
             double r3 = find_root(g3, 10, 0.1, 100, 1000, 0.000001);
@@ -109,37 +283,13 @@ namespace statphys
             Debug.Assert(Math.Abs(x3 - r3) < eps);
         }
 
-        double g1(double x)
-        {
-            return 1 - Math.Exp(-x);
-        }
-
-        double g2(double x)
-        {
-            return -(Math.Sin(x) + Math.Cos(x));
-        }
-        double g3(double x)
-        {
-            return Math.Exp(x*x);
-        }
-
-        //Численное интегрирование и тесты
-        double integrate_numeric(Func<double, double> f, double a, double b, int n)
-        {
-            double h = (b - a) / n;
-            double S = 0;
-            for (int i = 0; i < n; ++i)
-            {
-                double x1 = a + i * h;
-                double x2 = a + (i + 1) * h;
-                // Метод Симпсона https://habr.com/ru/post/479202/
-                S += h / 6.0 * (f(x1) + 4.0 * f((x1 + x2) / 2) + f(x2));
-            }
-            return S;
-        }
-
+        // Test numeric integral
         void test_integrator()
         {
+            Func<double, double> f1 = x => x;
+            Func<double, double> f2 = x => x * 2 + x;
+            Func<double, double> f3 = x => Math.Sin(x) * Math.Exp(-x) + Math.Cos(x);
+
             double r1 = integrate_numeric(f1, -0.5, 4, 1000);
             double r2 = integrate_numeric(f2, -0.5, 4, 1000);
             double r3 = integrate_numeric(f3, -0.5, 4, 1000);
@@ -152,50 +302,20 @@ namespace statphys
             Debug.Assert(Math.Abs(S3 - r3) < eps);
         }
 
-        double f1(double x)
-        {
-            return x;
-        }
-
-        double f2(double x)
-        {
-            return x * 2 + x;
-        }
-
-        double f3(double x)
-        {
-            return Math.Sin(x) * Math.Exp(-x) + Math.Cos(x);
-        }
-
         //sample test
         void test_sampling()
         {
-            const int N = 500;
+
+            const int N = 1000;
             List<double> arr = new List<double> { };
-            for(int i = 0; i < N; ++i)
+            for (int i = 0; i < N; ++i)
             {
                 arr.Add(sample_delta());
             }
             for (int i = 0; i < arr.Count(); ++i)
             {
-                Debug.Write($"{arr[i]}, ");
+                Trace.WriteLine(arr[i].ToString(CultureInfo.GetCultureInfo("en-GB")));
             }
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            test_integrator();
-            test_root();
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-            test_sampling();
-        }
-
-        private void button4_Click(object sender, EventArgs e)
-        {
-            timer1.Start();
         }
     }
 }
